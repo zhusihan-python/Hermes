3f4ccc1e7d2284df9f2d62cb7e477c0ead006a27
David Ascencio <david1906@outlook.com>
2024-08-07 12:16:01 -0600

    Dev: WIP

--- Hermes/Models/Settings.cs
+++ Hermes/Models/Settings.cs
@@ -10,9 +10,9 @@ public class Settings
     public int SfcTimeoutSeconds { get; set; } = 10;
     public int WaitDelayMilliseconds { get; set; } = 100;
     public int UutSuccessWindowTimeoutSeconds { get; set; } = 5;
-    public string BackupPath { get; set; } = @"C:\Users\david\Documents\Tmp\Hermes\Backup"; // TODO
+    public string BackupPath { get; set; } = @"C:\Users\Foxconn\Documents\Tpm\HermesLogfiles\Backup"; // TODO
     public FileExtension InputFileExtension { get; set; } = FileExtension._3dx; // TODO
-    public string InputPath { get; set; } = @"C:\Users\david\Documents\Tmp\Hermes\Input"; // TODO
-    public string SfcPath { get; set; } = @"C:\Users\david\Documents\Tmp\Hermes\Sfc"; // TODO
+    public string InputPath { get; set; } = @"C:\Users\Foxconn\Documents\Tpm\HermesLogfiles\Input"; // TODO
+    public string SfcPath { get; set; } = @"C:\Users\Foxconn\Documents\Tpm\HermesLogfiles\Sfc"; // TODO
     public int MaxSfcRetries { get; set; } = 3;
 }
