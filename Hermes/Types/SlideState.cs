ac9a39207be3643b79fbe8874350cbd9ace1a45e
allenz <1009698013@qq.com>
2024-12-03 14:31:59 +0800

    adjust UutProcessorView and simplify SlideState

--- Hermes/Types/SlideState.cs
+++ Hermes/Types/SlideState.cs
@@ -5,7 +5,6 @@ public enum SlideState
     Empty,          // 空位置
     NotSorted,      // 待整理
     Sorted,         // 已整理
-    Recognized,     // 已识别
     NotRecognized,  // 未识别
     SlideBlocked    // 卡片
 }
