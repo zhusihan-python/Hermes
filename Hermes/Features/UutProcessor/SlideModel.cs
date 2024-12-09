07bca7b852979f858cc3b407ede7e1873020c6fe
allenz <1009698013@qq.com>
2024-12-09 10:56:40 +0800

    adjust SlideBoxView MinWidth

--- Hermes/Features/UutProcessor/SlideModel.cs
+++ Hermes/Features/UutProcessor/SlideModel.cs
@@ -1,18 +1,17 @@
 ﻿using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using CommunityToolkit.Mvvm.Messaging;
-using Hermes.Common.Messages;
-using Hermes.Language;
 using Hermes.Models;
 using Hermes.Types;
-using R3;
+using Microsoft.EntityFrameworkCore.Update.Internal;
 
 namespace Hermes.Features.UutProcessor;
 public partial class SlideModel : ViewModelBase
 {
-    [ObservableProperty] private string _statusText = Resources.enum_empty;
+    [ObservableProperty] private int _rowIndex = 0;
+    [ObservableProperty] private bool _isSelected = false;
     [ObservableProperty] public Color backColor = Colors.White;
+
+    private Slide Slide { get; set; }
     private SlideState _state;
     public SlideState State
     {
@@ -31,37 +30,30 @@ public partial class SlideModel : ViewModelBase
         this.IsActive = true;
     }
 
+    public SlideModel(int SlideId)
+    {
+        //Slide.Initialize(SlideId);
+    }
+
     // 根据 SlideState 更新状态文本和背景颜色
     private void UpdateStatusAndColor()
     {
         switch (State)
         {
             case SlideState.Empty:
-                StatusText = Resources.enum_empty;
                 BackColor = Colors.White;
                 break;
             case SlideState.NotSorted:
-                StatusText = Resources.enum_not_sorted;
                 BackColor = Colors.Cyan;
                 break;
             case SlideState.Sorted:
-                StatusText = Resources.enum_sorted;
                 BackColor = Colors.Green;
                 break;
-            case SlideState.Recognized:
-                StatusText = Resources.enum_recognized;
-                BackColor = Colors.Blue;
-                break;
             case SlideState.NotRecognized:
-                StatusText = Resources.enum_not_recognized;
-                BackColor = Colors.Yellow;
-                break;
             case SlideState.SlideBlocked:
-                StatusText = Resources.enum_slide_blocked;
                 BackColor = Colors.Red;
                 break;
             default:
-                StatusText = Resources.enum_empty;
                 BackColor = Colors.Transparent;
                 break;
         }
