3a65c28e596ad6e8877cbf8fdc1a2b84615a10ad
allenz <1009698013@qq.com>
2024-12-10 11:29:21 +0800

    add SlideBoxDetailView and BoxInsertDialogVie

---
+++ Hermes/Features/UutProcessor/BoxInsertDialogViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.Threading;
+using CommunityToolkit.Mvvm.Input;
+using Hermes.Language;
+using Hermes.Models;
+using SukiUI.Dialogs;
+using System;
+
+namespace Hermes.Features.UutProcessor;
+
+public partial class BoxInsertDialogViewModel: ViewModelBase
+{
+    //public event Action<User>? Accepted;
+    private readonly ISukiDialog _dialog;
+    public BoxInsertDialogViewModel(ISukiDialog dialog)
+    {
+        this._dialog = dialog;
+    }
+
+    [RelayCommand]
+    private void Accept()
+    {
+        this.ShowSuccessToast("流程开始");
+    }
+
+
+    [RelayCommand]
+    public void CloseDialog()
+    {
+        Dispatcher.UIThread.InvokeAsync(() => _dialog.Dismiss());
+    }
+}
