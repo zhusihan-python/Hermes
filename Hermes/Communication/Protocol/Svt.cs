80966bf625380792bb69a2f3aadf776cf00dbb01
allenz <1009698013@qq.com>
2025-01-14 12:13:00 +0800

    SvtRequestInfo implement IRequestInfoBuilder

---
+++ Hermes/Communication/Protocol/Svt.cs
@@ -0,0 +1,63 @@
+﻿namespace Hermes.Communication.Protocol;
+
+/// <summary>
+///     Defines constants related to the Modbus protocol.
+/// </summary>
+internal static class Svt
+{
+    // frame head 0x3C 0x28 frame tail 0x29 0x3E
+    //帧头定义
+    public static readonly byte[] FullHead = { 0x3C, 0x28 };
+    //帧尾定义
+    public static readonly byte[] FullTail = { 0x29, 0x3E };
+    public const byte InsertStartByte = 0x82; //!0x3C
+    public const byte InsertEndByte = 0x83; //!0x3E
+    public const byte StartByte = 0x3C;  // "<"
+    public const byte StartByte1 = 0x28;  // "("
+    public const byte EndByte = 0x29;  // ")"
+    public const byte EndByte1 = 0x3E;  // ">"
+
+    // supported function codes
+    public const ushort DiagnosticsReturnQueryData = 0;
+    public const ushort DeviceId = 0x0020;
+    public const ushort BaudRate = 0x0022;
+    public const ushort Diagnostics = 0x0026;
+    public const ushort BakeParams = 0x0338;
+    public const ushort ScanTrigger = 0x033A;
+    public const ushort ScanResult = 0x033B;
+    public const ushort HeartBeat = 0x033F;
+    public const ushort SystemStatus = 0x0340;
+    public const ushort FlowAction = 0x0341;
+
+    public const int MaximumDiscreteRequestResponseSize = 65535;
+    public const int MaximumRegisterRequestResponseSize = 4095;
+
+    // supported extend codes
+    public const byte Read = 0x55;
+    public const byte Write = 0x66;
+    public const byte ReadSuccess = 0xAA;
+    public const byte WriteScueess = 0x88;
+    public const byte WriteFailed = 0x99;
+
+    // modbus slave exception offset that is added to the function code, to flag an exception
+    public const byte ExceptionOffset = 128;
+
+    // modbus slave exception codes
+    public const byte IllegalFunction = 1;
+    public const byte IllegalDataAddress = 2;
+    public const byte Acknowledge = 5;
+    public const byte SlaveDeviceBusy = 6;
+
+    // default setting for number of retries for IO operations
+    public const int DefaultRetries = 3;
+
+    // default number of milliseconds to wait after encountering an ACKNOWLEGE or SLAVE DEVIC BUSY slave exception response.
+    public const int DefaultWaitToRetryMilliseconds = 250;
+
+    // default setting for IO timeouts in milliseconds
+    public const int DefaultTimeout = 1000;
+
+    // used by the ASCII tranport to indicate end of message
+    // public const string NewLine = ")>";
+    public const string NewLine = "293E";
+}
--- Hermes/Communication/SerialPort/SvtDataHandlingAdapter.cs
+++ Hermes/Communication/SerialPort/SvtDataHandlingAdapter.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Hermes.Communication.Protocol;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using TouchSocket.Core;
 
@@ -9,13 +11,7 @@ public class SvtDataHandlingAdapter : CustomDataHandlingAdapter<SvtRequestInfo>
 {
     // 数据为空的包 最小长度为18
     public int HeaderLength = 18;
-    private const byte startByte = 0x3C;
-    private const byte endByte = 0x29;
-    private const byte insertStartByte = 0x82; //!0x3C
-    private const byte insertEndByte = 0x83; //!0x3E
-    private byte[] Headers = { 0x3C, 0x28 };
-    private byte[] Tails = { 0x29, 0x3E };
-    public override bool CanSendRequestInfo => false;
+    public override bool CanSendRequestInfo => true;
 
     /// <summary>
     /// 筛选解析数据。实例化的TRequest会一直保存，直至解析成功，或手动清除。
@@ -38,11 +34,11 @@ public class SvtDataHandlingAdapter : CustomDataHandlingAdapter<SvtRequestInfo>
 
         var span = byteBlock.Span.Slice(byteBlock.Position, byteBlock.CanReadLength);
         // 先找包尾，有可能有多个包尾，取第一个
-        var tailIndex = span.IndexOfFirst(0, span.Length, Tails);
+        var tailIndex = span.IndexOfFirst(0, span.Length, Svt.FullTail);
         if (tailIndex > 0)
         {
             // 从包尾往前找包头
-            var headIndexes = span.IndexOfInclude(0, tailIndex - Tails.Length, Headers);
+            var headIndexes = span.IndexOfInclude(0, tailIndex - Svt.FullTail.Length, Svt.FullHead);
             // 找到取第一个
             if (headIndexes.Count > 0)
             {
@@ -50,7 +46,7 @@ public class SvtDataHandlingAdapter : CustomDataHandlingAdapter<SvtRequestInfo>
                 var lastHeadIndex = headIndexes[^1];
                 var pos = byteBlock.Position;//记录初始游标位置，防止本次无法解析时，回退游标。
                 // 去掉包头包尾
-                var package = span.Slice(lastHeadIndex+1, tailIndex+1 - Tails.Length -Headers.Length);
+                var package = span.Slice(lastHeadIndex+1, tailIndex+1 - Svt.FullTail.Length - Svt.FullHead.Length);
                 // 去掉转义字符，才能校验包长度和CRC
                 var cleanPackage = RemoveInsertedBytes(package);
                 var myRequestInfo = new SvtRequestInfo();
@@ -149,12 +145,12 @@ public class SvtDataHandlingAdapter : CustomDataHandlingAdapter<SvtRequestInfo>
             // 检查是否需要跳过下一个字节
             if (i + 1 < data.Length)
             {
-                if (data[i] == startByte && data[i + 1] == insertStartByte)
+                if (data[i] == Svt.StartByte && data[i + 1] == Svt.InsertStartByte)
                 {
                     // 跳过 0x82
                     i++;
                 }
-                else if (data[i] == endByte && data[i + 1] == insertEndByte)
+                else if (data[i] == Svt.EndByte && data[i + 1] == Svt.InsertEndByte)
                 {
                     // 跳过 0x83
                     i++;
@@ -167,17 +163,18 @@ public class SvtDataHandlingAdapter : CustomDataHandlingAdapter<SvtRequestInfo>
     }
 }
 
-public class SvtRequestInfo : IRequestInfo
+public class SvtRequestInfo : IRequestInfo, IRequestInfoBuilder
 {
-    private byte[] m_sync;
     private byte[] m_cMDID;
     private byte[] m_data;
     private byte[] m_cRC16;
 
+    public int MaxLength => this.DataLength + 20;
+
     /// <summary>
     /// 报文头: 固定为2位 值为0x3C0x28 字符表示为"<("
     /// </summary>
-    public byte[] Sync { get => this.m_sync; set => this.m_sync = value; }
+    public byte[] Head { get; set; }
 
     /// <summary>
     /// 报文序号: 固定为2位 由主设备指定，从设备在回复命令时，帧序号需与接收到的命令中的保持一致
@@ -217,7 +214,7 @@ public class SvtRequestInfo : IRequestInfo
     /// <summary>
     /// 数据长度: 固定2位 数据段的长度
     /// </summary>
-    public int DataLength { get; set; }
+    public ushort DataLength { get; set; }
 
     /// <summary>
     /// 数据区: 长度为BodyLength的Value
@@ -232,5 +229,87 @@ public class SvtRequestInfo : IRequestInfo
     /// <summary>
     /// 报文尾: 固定2位 值为0x290x3E 字符表示为")>"
     /// </summary>
-    public byte End { get; set; }
+    public byte Tail { get; set; }
+
+    public byte[] DataFrame()
+    {
+        // 计算总长度
+        ushort totalLength = (ushort)(12 + DataLength);
+        this.PacketLength = (ushort)(totalLength + 6);
+
+        // 使用 MemoryStream 和 BinaryWriter 连接数据
+        using (var memoryStream = new MemoryStream(totalLength))
+        using (var writer = new BinaryWriter(memoryStream))
+        {
+            // 写入 FrameNo（2 字节）
+            writer.Write(FrameNo);
+
+            // 写入 PacketLength（2 字节）
+            writer.Write(PacketLength);
+
+            // 写入 AddressLength（1 字节）
+            writer.Write(AddressLength);
+
+            // 写入 MasterAddress（1 字节）
+            writer.Write(MasterAddress);
+
+            // 写入 SlaveAddress（1 字节）
+            writer.Write(SlaveAddress);
+
+            // 写入 CMDID（2 字节）
+            writer.Write(CMDID);
+
+            // 写入 FrameType（1 字节）
+            writer.Write(FrameType);
+
+            // 写入 DataLength（2 字节）
+            writer.Write(BitConverter.GetBytes(DataLength).Reverse().ToArray());
+
+            // 写入 Data（可变长度）
+            if (Data != null && Data.Length > 0)
+            {
+                writer.Write(Data);
+            }
+
+            // 返回最终的字节数组
+            return memoryStream.ToArray();
+        }
+    }
+
+    public void Build<TByteBlock>(ref TByteBlock byteBlock) where TByteBlock : IByteBlock
+    {
+        var msgFrame = this.DataFrame();
+        var crcAscii = Crc16.ComputeCrcArray(msgFrame, msgFrame.Length);
+
+        byteBlock.Write(Svt.FullHead);
+        byteBlock.Write(AddSymbol(msgFrame));
+        byteBlock.Write(AddSymbol(crcAscii));
+        byteBlock.Write(Svt.FullTail);
+    }
+
+    public static ReadOnlySpan<byte> AddSymbol(byte[] inputArray)
+    {
+        if (inputArray == null || inputArray.Length == 0)
+        {
+            // 如果数组为空或长度为0，直接返回原数组
+            return inputArray;
+        }
+        int count = 0;
+        byte[] packBuffer = new byte[inputArray.Length * 2];
+
+        //如果协议帧包含关键字，添加隔断符
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            packBuffer[count++] = inputArray[i];
+            if (inputArray[i] == Svt.StartByte)
+            {
+                packBuffer[count++] = Svt.InsertStartByte;
+            }
+            if (inputArray[i] == Svt.EndByte)
+            {
+                packBuffer[count++] = Svt.InsertEndByte;
+            }
+        }
+        return packBuffer.AsSpan(0, count);
+    }
 }
