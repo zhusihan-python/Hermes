d31e8c5e5ffd60bb80706cb422c091e91ecea710
allenz <1009698013@qq.com>
2025-02-10 09:41:09 +0800

    add enum FlowType

---
+++ Hermes/Types/FlowType.cs
@@ -0,0 +1,11 @@
+﻿namespace Hermes.Types;
+
+public enum FlowType
+{
+    Start,          // 空位置
+    Cancel,         // 取消
+    Delay,          // 延迟
+    Pause,          // 待整理
+    Resume,         // 已整理
+    Stop,           // 停止
+}
