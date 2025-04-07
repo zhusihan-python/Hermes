c7263b4c7bdea777f1fb131527df0e4a26609911
allen zhu <1009698013@qq.com>
2025-04-07 11:37:26 +0800

    add ScanCode HeartBeat

---
+++ Hermes/Communication/SerialPort/Message/HeartBeat.cs
@@ -0,0 +1,13 @@
+﻿using Hermes.Communication.Protocol;
+
+namespace Hermes.Communication.SerialPort;
+
+public class HeartBeatRead : SvtRequestInfo
+{
+    public HeartBeatRead()
+    {
+        this.CMDID = Svt.HeartBeat;
+        this.FrameType = Svt.Read;
+        this.DataLength = 0;
+    }
+}
---
+++ Hermes/Communication/SerialPort/Message/ScanCode.cs
@@ -0,0 +1,44 @@
+﻿using Hermes.Communication.Protocol;
+
+namespace Hermes.Communication.SerialPort;
+
+public class ScanTriggerWriteResponse : SvtRequestInfo
+{
+    public ScanTriggerWriteResponse()
+    {
+        this.CMDID = Svt.ScanTrigger;
+        this.FrameType = Svt.WriteResponse;
+        this.DataLength = 1;
+    }
+
+    public SvtRequestInfo TriggerSuccess()
+    {
+        return WithData<ScanTriggerWriteResponse>(new byte[] { Svt.WriteScueess });
+    }
+
+    public SvtRequestInfo TriggerFail()
+    {
+        return WithData<ScanTriggerWriteResponse>(new byte[] { Svt.WriteFailed });
+    }
+}
+
+public class ScanResultWrite : SvtRequestInfo
+{
+    public ScanResultWrite(byte[] frameNumber)
+    {
+        this.FrameNo = frameNumber;
+        this.CMDID = Svt.ScanResult;
+        this.FrameType = Svt.Write;
+        this.DataLength = 1;
+    }
+
+    public SvtRequestInfo ScanSuccess()
+    {
+        return WithData<ScanResultWrite>(new byte[] { 0x01 });
+    }
+
+    public SvtRequestInfo ScanFail()
+    {
+        return WithData<ScanResultWrite>(new byte[] { 0x02 });
+    }
+}
