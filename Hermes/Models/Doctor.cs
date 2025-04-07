8c8eaea901ff6d4df9c8dcb837cd14041e4e9953
allen zhu <1009698013@qq.com>
2025-04-07 11:39:36 +0800

    add Doctor Slide model

---
+++ Hermes/Models/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hermes.Models;
+
+public class Doctor: ObservableObject
+{
+    public static readonly Doctor Null = new NullDoctor();
+    [Key] public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public string Department { get; set; }
+    [NotMapped] public bool IsNull => this == Null;
+
+    public class NullDoctor : Doctor { }
+}
+
---
+++ Hermes/Models/Slide.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using static Hermes.Models.Doctor;
+
+namespace Hermes.Models;
+
+public partial class Slide : ObservableObject
+{
+    public static readonly Slide Null = new NullSlide();
+    [Key] public int Id { get; set; }
+
+    [MaxLength(64)]
+    public string ProgramId { get; set; } = string.Empty;
+    public int PathologyId { get; set; }
+    [Required]
+    [MaxLength(16)]
+    public int SlideId { get; set; }
+    [MaxLength(64)]
+    public string PatientName { get; set; } = string.Empty;
+    public int DoctorId { get; set; }  // 外键
+    public Doctor? Doctor { get; set; }  // 导航属性
+    public string EntryDate { get; set; } = string.Empty;
+    public class NullSlide : Slide { }
+}
