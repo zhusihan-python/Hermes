1859fc7bb501368935d1e7c51b7b193845b82f07
allen zhu <1009698013@qq.com>
2025-04-22 16:32:07 +0800

    remove StopRepository

--- Hermes.IntegrationTests/Repositories/StopRepositoryTests.cs
+++ Hermes.IntegrationTests/Repositories/StopRepositoryTests.cs
@@ -8,7 +8,7 @@ namespace HermesIntegrationTests.Repositories;
 
 public class StopRepositoryTests
 {
-    private readonly StopRepository _sut;
+    //private readonly StopRepository _sut;
     private readonly HermesLocalContext _localContext;
     private readonly SfcResponseBuilder _sfcResponseBuilder;
 
@@ -19,20 +19,20 @@ public class StopRepositoryTests
         this._sfcResponseBuilder = sfcResponseBuilder;
         this._localContext = contextFactory.CreateDbContext();
         _localContext.Database.EnsureCreated();
-        this._sut = new StopRepository(contextFactory);
+        //this._sut = new StopRepository(contextFactory);
     }
 
-    [Fact]
-    public async Task GetConsecutiveSameDefects_WithConsecutiveDefects_ReturnsDefect()
-    {
-        var sfcResponse = _sfcResponseBuilder.Build();
-        await _localContext.SfcResponses.AddAsync(sfcResponse);
-        await _localContext.SaveChangesAsync();
-        var stop = new Stop(StopType.Machine);
-        await this._sut.AddAndSaveAsync(stop);
-        await this._sut.RestoreAsync(stop, new List<User>());
-        var result = await this._sut.GetById(stop.Id);
-        Assert.True(result?.IsRestored);
-        Assert.NotEqual(0, result?.Id);
-    }
+    //[Fact]
+    //public async Task GetConsecutiveSameDefects_WithConsecutiveDefects_ReturnsDefect()
+    //{
+    //    var sfcResponse = _sfcResponseBuilder.Build();
+    //    await _localContext.SfcResponses.AddAsync(sfcResponse);
+    //    await _localContext.SaveChangesAsync();
+    //    var stop = new Stop(StopType.Machine);
+    //    await this._sut.AddAndSaveAsync(stop);
+    //    await this._sut.RestoreAsync(stop, new List<User>());
+    //    var result = await this._sut.GetById(stop.Id);
+    //    Assert.True(result?.IsRestored);
+    //    Assert.NotEqual(0, result?.Id);
+    //}
 }
