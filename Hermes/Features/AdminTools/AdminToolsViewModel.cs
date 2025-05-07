609794f221b327957c9f25300687189179e10305
allenz <1009698013@qq.com>
2025-05-07 17:38:32 +0800

    try fix Unable to cast object of type AdminToolsViewModel to type AboutTabViewModel

--- Hermes/Features/AdminTools/AdminToolsViewModel.cs
+++ Hermes/Features/AdminTools/AdminToolsViewModel.cs
@@ -1,12 +1,15 @@
-using Hermes.Language;
 using Material.Icons;
 
 namespace Hermes.Features.AdminTools;
 
-public class AdminToolsViewModel(FeaturePermissionsViewModel featurePermissionsViewModel) : PageBase(
+public class AdminToolsViewModel(
+    FeaturePermissionsViewModel featurePermissionsViewModel,
+    AboutTabViewModel aboutTabViewModel
+    ) : PageBase(
     "设置",
     MaterialIconKind.HelpCircle,
     98)
 {
     public FeaturePermissionsViewModel FeaturePermissionsViewModel { get; } = featurePermissionsViewModel;
+    public AboutTabViewModel AboutTabViewModel { get; } = aboutTabViewModel;
 }
