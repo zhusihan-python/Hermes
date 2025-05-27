4217fe66ccd5dfd03c2ba38a75132c966b60111c
allenz <1009698013@qq.com>
2025-05-27 15:39:08 +0800

    add DoctorToTextConverters

---
+++ Hermes/Common/Converters/DoctorToTextConverters.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+using Hermes.Language;
+using Hermes.Models;
+
+namespace Hermes.Common.Converters;
+
+public static class DoctorToTextConverters
+{
+    public static readonly DoctorToTextConverter<Doctor> Generic = new();
+}
+
+public class DoctorToTextConverter<T>() : IValueConverter where T : Doctor
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null) return Resources.txt_all;
+        return value is not T doc ? default(object?) : doc.Name;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
