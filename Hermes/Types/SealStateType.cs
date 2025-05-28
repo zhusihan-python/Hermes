ecd83c581ebfc8c2456134a9f2410437f1f48dc1
allenz <1009698013@qq.com>
2025-05-28 15:09:59 +0800

    add SealStateType SortStateType

---
+++ Hermes/Types/SealStateType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Hermes.Types;
+
+public enum SealStateType
+{
+    [Description("未封片")]
+    SlideNotSeal,
+    [Description("已封片")]
+    SlideSealed,
+}
---
+++ Hermes/Types/SortStateType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Hermes.Types;
+
+public enum SortStateType
+{
+    [Description("未理片")]
+    SlideNotSort,
+    [Description("已理片")]
+    SlideSorted,
+}
